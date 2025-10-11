3e0520e0ad1954e9e0d07ea45e933db4c7eea690
Gitanjali Sase <sasegitanjali20@gmail.com>
2025-10-11 13:45:18 +0530

    Remove .env from repo and add to .gitignore

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'molarmap',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASS || 'root', {
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS, {
   host: 'localhost',  // or your DB host
   dialect: 'mysql',
   port: process.env.DB_PORT || 3306,         // change port if needed
